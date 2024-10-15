1991df894f947920c0139d11e7ed76027a98b97c
Ariel Mitchell <ariel.l.makowski@gmail.com>
2024-10-14 21:58:54 -0400

    user signup studio complete with bonus missions

--- src/main/java/org/launchcode/controllers/UserController.java
+++ src/main/java/org/launchcode/controllers/UserController.java
@@ -1,9 +1,42 @@
 package org.launchcode.controllers;
 
+import org.launchcode.data.UserData;
+import org.launchcode.models.User;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
 
 @Controller
 @RequestMapping("user")
 public class UserController {
+
+    @GetMapping("add")
+    public String displayAddUserForm() {
+        return "user/add";
+    }
+
+    @PostMapping
+    public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
+
+        if (verify.equals(user.getPassword())) {
+            UserData.add(user);
+            Collection<User> users = UserData.getAll();
+            model.addAttribute("users", users);
+            return "user/index";
+        } else {
+            model.addAttribute("username", user.getUsername());
+            model.addAttribute("email", user.getEmail());
+            model.addAttribute("error", "Passwords must match");
+            return "user/add";
+        }
+    }
+
+    @GetMapping("detail/{userId}")
+    public String displayUserDetails(Model model, @PathVariable int userId) {
+        model.addAttribute("user", UserData.getById(userId));
+        return "user/detail";
+    }
+
 }
---
+++ src/main/java/org/launchcode/models/User.java
@@ -0,0 +1,62 @@
+package org.launchcode.models;
+
+import java.util.Date;
+
+public class User {
+
+    private String username;
+    private String email;
+    private String password;
+    private int id;
+    private static int nextId = 1;
+    private Date dateCreated;
+
+    public User() {
+        this.dateCreated = new Date();
+        this.id = nextId;
+        nextId++;
+    }
+
+    public User(String username, String email, String password) {
+        this();
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(Date dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+}
